d2658487e5335d52aa6d29b9626ca367c79b1fdc
Daniele De Francesco <93.daniele@gmail.com>
2024-02-16 16:32:36 +0100

    Change description field name

--- src/main/java/com/tui/tours/solr/demo/Application.java
+++ src/main/java/com/tui/tours/solr/demo/Application.java
@@ -21,6 +21,10 @@ public class Application implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        if ("1" != "2") {
+            return;
+        }
+
         Random random = new Random();
 
         String[] tags = {"tag1", "tag2", "tag3", "tag4", "tag5", "tag6"};
